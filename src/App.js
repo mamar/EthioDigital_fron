68a3d24af02e34123a463587281206bcc7fea3dc
Mamar Habitamu <hmamar@abaybank.local>
2021-12-30 08:37:36 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState,useEffect } from 'react';
+import axios from 'axios';
 function App() {
+
+  const [media_Name,setmedia_Name]=useState("")
+  const submitMedia=()=>{
+    axios.post('http://127.0.0.1:8080/AddMedia',{media_Name:media_Name}).then(()=>{
+      setMediaList([medilist,{media_Name:media_Name}])
+    });
+    
+
+  }
+  const [newmedia,setnewmedia]=useState("")
+  const deletReview=(media_Name)=>{
+    axios.delete('http://127.0.0.1:8080/DeletMedia/',{
+      media_Name:media_Name
+    }).then(()=>{
+
+    })
+  }
+  const updateReview =(movie)=>{
+    axios.put("http://127.0.0.1:8080/UpdateMedia",{
+      media_Name:media_Name
+    });
+    setnewmedia("")
+  }
+  const [medilist,setMediaList]=useState([]);
+  useEffect(()=>{
+    axios.get('http://127.0.0.1:8080/GetMedia').then((Response)=>{
+      setMediaList(Response.data)
+    })
+  })
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>  Crud Application</h1> 
+     <div className='form'>
+     <label>Media Name</label>
+     <input type="text" name='media_Name' onChange={(e)=>setmedia_Name(e.target.value)}></input>
+     <button onClick={submitMedia}>Submit</button>
+       
+      {medilist.map((val)=>{
+        return(
+          <div className='card'> 
+       <h1>{val.media_Name}</h1>
+        <button onClick={()=>{
+          deletReview(val.media_Name)
+        }}>Delete</button>
+        <input  type="text"  id ="updateInput" onChange={(e)=>
+        {
+          setnewmedia(e.target.value)
+        }}></input>
+        <button onClick={()=>{updateReview(val.media_Name)}}>Update </button>
+        </div>
+        )
+      })};
+     
+         
+     
+    
+     </div>
+    
+     
     </div>
   );
 }
